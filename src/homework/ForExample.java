f7b9d3df25b4439317958394ba75776572b8f12e
Gevorg <gevorgavetisyan994@gmail.com>
2022-12-20 15:24:12 +0400

    Test commit message

--- src/homework/ForExample.java
+++ src/homework/ForExample.java
@@ -2,7 +2,7 @@ package homework;
 
 public class ForExample {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for(int i = 0; i < 5; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print(" #");
             }
---
+++ src/homework1/IfForExample.java
@@ -0,0 +1,31 @@
+package homework1;
+
+import java.sql.SQLOutput;
+
+public class IfForExample {
+    public static void main(String[] args) {
+        int a = 27;
+        int b = 43;
+        if (a < b) {
+            System.out.println("max :" + b);
+        }
+        if (a > b) {
+            System.out.println("max :" + a);
+        }
+
+        for (a = 27; a <= 43; a++) {
+            System.out.print(a + " ,");
+        }
+        System.out.println();
+
+        char x = 27;
+        char y = 43;
+        System.out.println(x);
+        System.out.println(y);
+        if (x == y){
+        System.out.println("x equal y");}else {
+            System.out.println("x no equal y");}
+        }
+    }
+
+
